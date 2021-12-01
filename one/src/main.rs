005c5d245f8d19c83d353b64ed54877ca6f08908
Trevor McDonald <tmcdonald@breadfinance.com>
2021-12-01 11:22:09 -0600

    Beautify

--- one/src/main.rs
+++ one/src/main.rs
@@ -1,34 +1,27 @@
 use std::fs;
 
+fn get_increases(acc: (i32, i32), x: &i32) -> (i32, i32) {
+    if x > &acc.0 {
+        (x.to_owned(), acc.1 + 1)
+    } else {
+        (x.to_owned(), acc.1)
+    }
+}
+
 fn main() {
     let contents = fs::read_to_string("input.txt").unwrap();
-    let lines = contents.lines();
-    let answer = lines.fold((i64::MAX, 0), |acc, x| {
-        let parsed_value = x.parse::<i64>().unwrap();
-        if parsed_value > acc.0 {
-            (parsed_value, acc.1 + 1)
-        } else {
-            (parsed_value, acc.1)
-        }
-    });
-    println!("{:?}", answer);
 
-    // Part Deux
-    let ints: Vec<i64> = contents
+    let ints: Vec<i32> = contents
         .lines()
-        .map(|x| x.parse::<i64>().unwrap())
+        .map(|x| x.parse::<i32>().unwrap())
         .collect();
 
-    let answer_two = ints
-        .windows(3)
-        .map(|w| w.iter().sum())
-        .fold((i64::MAX, 0), |acc, x| {
-            if x > acc.0 {
-                (x, acc.1 + 1)
-            } else {
-                (x, acc.1)
-            }
-        });
+    // Part Uno
+    let answer = ints.iter().fold((i32::MAX, 0), get_increases);
+    println!("One: {:?}", answer.1);
 
-    println!("{:?}", answer_two);
+    // Part Deux
+    let windows: Vec<i32> = ints.windows(3).map(|w| w.iter().sum::<i32>()).collect();
+    let answer_two = windows.iter().fold((i32::MAX, 0), get_increases);
+    println!("Two: {:?}", answer_two.1);
 }
